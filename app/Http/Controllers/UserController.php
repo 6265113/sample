b1c27956bad0b3ed65ef918cd9a51080355d4394
6265113 <6265113@gmail.com>
2018-10-12 09:55:21 +0000

    Add password resets & email configuration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -96,12 +96,10 @@ class UserController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'sharetrip@qq.com';
-        $name = 'sharetrip';
         $to = $user->email;
         $subject = '感谢注册 sample 应用！请确认你的邮箱。';
-        Mail::send($view, $data, function($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 
